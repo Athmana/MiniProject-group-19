d934ef7e7658c08c1ac8464bf5dfe68916c3c57d
nandu200577 <nandima73@gmail.com>
2026-03-05 08:46:42 +0530

    Updated project files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.gowayanad"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
